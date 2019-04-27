42a0a7a8861b2f9d5d1a200870c63a4546d4f514
Oliver Trotter <oliver@hellodigital.nz>
2019-04-28 00:26:34 +1200

    fix

--- db_connect.php
+++ db_connect.php
@@ -3,10 +3,12 @@ function db() {
   global $link;
   $link = mysqli_connect(“localhost”, “todouser”, “todo123”, “todolist”) or     die(“couldn’t connect to database”);
   return $link;
-  if(db()) {
-    echo “wawu !!! I’m connected”;
-  } else {
-    echo "oh no";
-  }
 }
+
+if(db()) {
+  echo “wawu !!! I’m connected”;
+} else {
+  echo "oh no";
+}
+
 ?>
